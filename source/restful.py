bae716a028d58e55fe40e2bdcd7d0afba1b839e9
fanlia <you@example.com>
2022-12-14 07:06:45 +0800

    first commit

--- source/restful.py
+++ source/restful.py
@@ -1,5 +1,11 @@
 
+from models.test import test
+
 def restful(app):
     @app.route('/hello')
     def hello_world():
        return "hello world"
+
+    @app.route('/test')
+    def test_route():
+       return test()
