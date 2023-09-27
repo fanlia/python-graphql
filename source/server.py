d95bc3eff2a44c6546f46d0299ea621fb9c60696
odootest <odootest2023@163.com>
2023-09-27 15:17:38 +0800

    refine

--- source/server.py
+++ source/server.py
@@ -1,6 +1,8 @@
 
+import time
+import sys
 from waitress import serve
 from app import app, port
  
-print("server started at http://localhost:{}".format(port))
+print("server started at http://localhost:{} [{}][python:{}]".format(port, time.asctime(), sys.version))
 serve(app, host='0.0.0.0', port=port)
